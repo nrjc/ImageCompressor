775e33dffb08bf6afa733c442030a4bc3e7ab5de
Nicholas Capel <nrjc2@cam.ac.uk>
2016-05-23 11:49:38 +0100

    Added some code for REPORT 2

--- calculatedeltarmserror4.m
+++ calculatedeltarmserror4.m
@@ -1,6 +1,6 @@
-function rmserror=calculatedeltarmserror4(originalimage,dwtstep,refstepsize,rise)
+function rmserror=calculatedeltarmserror4(originalimage,dwtstep,refstepsize,ratio)
     [m n]=size(dwtstep);
-    [reconstimage,entropy]=quantdwt(originalimage,dwtstep,rise);
+    [reconstimage,entropy]=quantdwt(originalimage,dwtstep,ratio);
     reconstimage = nlevidwt(reconstimage,n);
     rmserror1=std(originalimage(:)-reconstimage(:));
     reconstimage2=quantise(originalimage,refstepsize);
--- saveanddisplay6.m
+++ saveanddisplay6.m
@@ -5,27 +5,30 @@ map = evalin('base', 'map');
 ein = bpp(quantise(imagein,17))*mini*nin;
 %Generate constant step size scheme
 %imwrite((quantisedimage-128*round(min(quantisedimage(:))/128)),map,['stepsize' num2str(step) '.png']);
-
+i=1;
 %INVESTIGATE THE LBT
 %Plot CR for different ratios, while keeping RMS constant. 
-for ratio=0.1:0.1:10
-    fun1=@(x)calculateerrorLBT(imagein,sqrt(2),x,17,x*ratio);
-    [step f]=fminbnd(fun1,0,50);
-    Xp = LBT(imagein,8,sqrt(2));
-    dctcell = dctenc(Xp,dct_ii(8));
-    compressionratio(i)=calculatecompression2(imagein,dctcell,step,8,17,step*ratio);
-    i=i+1;
-end
+% for ratio=0.1:0.1:3
+%     fun1=@(x)calculateerrorLBT(imagein,sqrt(2),x,17,x*ratio);
+%     [step f]=fminbnd(fun1,0,50);
+%     Xp = LBT(imagein,8,sqrt(2));
+%     dctcell = dctenc(Xp,dct_ii(8));
+%     compressionratio(i)=calculatecompression2(imagein,dctcell,step,16,17,step*ratio);
+%     i=i+1;
+% end
 %These results are a little suspicious, as they do not agree with the
 %original results
-
-
+i=1;
+for ratio=0.1:0.1:3
 %INVESTIGATE THE DWT
 %Golden Search to find the ideal rise size.
-% dwtstepm=generatedwtstep(6);
-% fun2=@(x)calculatedeltarmserror4(imagein,x*dwtstepm,17);
-% [idealstepdwt f]=fminbnd(fun2,1000,10000);
-
+    dwtstepm=generatedwtstep(6);
+    fun2=@(x)calculatedeltarmserror4(imagein,x*dwtstepm,17,ratio);
+    [idealstepdwt(i) f(i)]=fminbnd(fun2,100,30000);
+    [quantisedimage2,dwtentk]=quantdwt(imagein,idealstepdwt(i)*dwtstepm,ratio);
+    compressionratio(i)=ein/sum(dwtentk(:));
+    i=i+1
+end
 
 end
  
